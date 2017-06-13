68b138f2e36933ca1cbe1e6bd2a7aa52c3f50b5d
Hong Man Hou <hoou8547@hotmail.com>
2017-06-13 14:33:39 +0800

    Add files via upload
    Change of the Router API. Added binary searching scheme Solver.

--- src/Common/Router.cpp
+++ src/Common/Router.cpp
@@ -1,1 +1,10 @@
 #include "Router.h"
+
+void Router::set(int n, int m, int k) {
+    N = n; M = m; K = k;
+    DN = n * (k + 1) + k; DM = m * (k + 1) + k; DMN = DM * DN;
+}
+
+void Router::reset() {
+    N = M = K = DN = DM = DMN = 0;
+}
---
+++ src/Common/Solver.cpp
@@ -0,0 +1,44 @@
+#include "Solver.h"
+#include "../NetWorkFlow/NFRouter.h"
+#include <iostream>
+
+using namespace std;
+
+Board* Solver::solve(int m,int n,RouterType type)
+{
+    int scale = (m > n) ? m : n;
+    int l = scale / 4, r = scale / 2;
+    int mid = (l + r) / 2;
+    Router* router = NULL;
+    switch(type)
+    {
+        case NF:
+            router = new NFRouter();
+            break;
+        case AS:
+            //router = new ASRouter(m, n, mid);
+            break;
+        default:
+            router = new NFRouter();
+            break;
+    }
+
+    while(l < r)
+    {
+
+        cout <<"( " <<l <<" , " <<mid <<" , " <<r <<" ) "<<endl;
+        router -> set(n, m, mid);
+
+        if(router->OK()) {
+            r = mid;
+        }
+        else l = mid + 1;
+        mid = (l + r) /2;
+    }
+    cout <<"binary search complete!" <<endl;
+    cout <<"shortest is " <<l <<endl;
+    router -> set(n, m , l);
+    Board* b = router->route();
+    delete router;
+    return b;
+}
